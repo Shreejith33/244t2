4038c96545bb6456107f378671050a1faee79a40
Shreejith33 <93464358+Shreejith33@users.noreply.github.com>
2023-01-23 19:26:26 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+noseX = 0;
+noseY = 0;
+
+function preload()
+{
+  mustache = loadImage('https://i.postimg.cc/0ykLzvrv/Mustache-removebg-preview.png');
+}
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide()
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet is Initialized')
+}
+
+function draw()
+{
+    image(video, 0, 0, 300, 300);
+    image(mustache, noseX, noseY, 50, 30);
+}
+
+function take_snapshot()
+{
+    save('MyMustacheImage.png')
+}
+
+function gotPoses (results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x - 23;
+        noseY = results[0].pose.nose.y + 3;
+        console.log("nose x =" + results[0].pose.nose.x);
+        console.log("nose y =" + results[0].pose.nose.y);
+    }
+}
